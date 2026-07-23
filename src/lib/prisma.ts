529b4bb21745b3f8a544161d32920804b5ef2e6d
Sounak <sounakchakraborty371@gmail.com>
2026-07-23 19:10:48 +0530

    fix: remove prisma pg adapter to fix vercel serverless connections

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
-
-const connectionString = `${process.env.DATABASE_URL}`;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 export default prisma;
